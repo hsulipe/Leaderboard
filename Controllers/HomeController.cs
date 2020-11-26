775fe942ead1062aacebf343f58a3d214e14881c
José Henrique <jh.henri7@gmail.com>
2020-11-26 18:09:05 -0300

    start project and endpoint to create a player

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LeaderboardApi.Models;
+using StackExchange.Redis;
+namespace leaderboard.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class PlayerController : Controller
+    {
+        [HttpPost]
+        public JsonResult Index([FromBody] Player player)
+        {
+            this.HttpContext.Response.StatusCode = 201;
+            return Json(player.save());
+        }
+
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
---
+++ Models/player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+using StackExchange.Redis;
+
+namespace LeaderboardApi.Models
+{
+    public class Player
+    {
+        
+        public string Nickname { get; set; }
+        public int Score { get; set; }
+
+        public Player(string nickname, int score)
+        {
+            this.Nickname = nickname;
+            this.Score = score;
+        }
+
+        
+        public Player save()
+        {
+            ConnectionMultiplexer muxer = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase conn = muxer.GetDatabase();
+            conn.StringSet(this.Nickname, this.Score);
+            return this;
+        }
+
+       
+    }
+}
